ce00520fdd3486a4cd9dbe38e2645635cac9d7f1
huan <namhocvuive@gmail.com>
2026-09-19 00:50:43 +0700

    feat(frontend): đăng ký route /image trong App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import TranslatorPage from "./pages/TranslatorPage";
+import ImageTranslatorPage from "./pages/ImageTranslatorPage";
 
 function App() {
+  return <ImageTranslatorPage />; //chạy rieng dịch ảnh
   return <TranslatorPage />;
 }
 
